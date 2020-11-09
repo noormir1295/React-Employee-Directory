180d80f325cd9a3b1ccf170462fba9ba78c80250
Noor <noormir1295@gmail.com>
2020-11-09 14:23:44 -0500

    adding navbar

--- employee-app/src/App.js
+++ employee-app/src/App.js
@@ -1,10 +1,17 @@
-
-import './App.css';
+import React from "react";
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     
+      <Router>
+        <div>
+          <Navbar />
+        </div>
+      
+      </Router>
+    
     </div>
   );
 }
