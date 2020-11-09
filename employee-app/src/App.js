dbb250774556730cb2f48d32244d20ef85f711a5
Noor <noormir1295@gmail.com>
2020-11-09 16:08:59 -0500

    adding employee files

--- employee-app/src/App.js
+++ employee-app/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Wrapper from "./components/Wrapper";
 import About from './pages/About';
+import Employees from './pages/Employees';
 
 
 function App() {
@@ -12,12 +13,12 @@ function App() {
         <div>
           <Navbar />
           <Wrapper>
-            <Route exact path='/' component={About} />
+            <Route exact path="/" component={About} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/all" component={Employees} />
           </Wrapper>
         </div>
-      
       </Router>
-    
     </div>
   );
 }
---
+++ employee-app/src/pages/Employees.js
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import Card from "../components/Card";
+import Row from "../components/Row";
+import Container from "../components/Container";
+import Column from "../components/Column";
+import "./styles.css";
+
+
+    class Employees extends Component {
+        state= {
+            allResults: [],
+            allResultsParsed:[],
+            results: []
+        };
+         componentDidMount() {
+    API.findPeople().then(res => {
+      this.setState({
+        allResults: res.data.results,
+        allResultsParsed: res.data.results,
+        results: res.data.results
+      });
+    });
+  }
+
+  handleSearchFirst= (event) => {
+      const firstName = event.target.value;
+      this.sortPeopleFirst(firstName);
+  }
+  handleSearchLast= (event)=> {
+      const lastName = event.target.value;
+      this.sortPeopleLast(lastName);
+  }
+    sortPeopleFirst = (value) => {
+    let searchResults = this.state.allResults.filter((x) => { 
+      return x.name.first.toLowerCase().includes(value);
+    }) 
+    this.setState({
+      allResultsParsed: searchResults
+    })
+  }
+  //function to sort by last name 
+    sortPeopleLast = (value) => {
+    let searchResults = this.state.allResults.filter((x) => { 
+      return x.name.last.toLowerCase().includes(value)
+    }) 
+    this.setState({
+      allResultsParsed: searchResults
+    })
+  }
+  render() {
+      const results= this.state.allResultsParsed;
+      return(
+      <div>
+          <h1 className='text-center'>Noor LLC Employees</h1>
+          <form className='search-form form-inline'>
+              <div>
+                  <input className="employeeSearch" placeholder="Search for Employee's First Name" onChange={this.handleSearchFirst}/>
+                    <input className="employeeSearch" placeholder="Search for Employee's Last Name" onChange={this.handleSearchLast}/>
+              </div>
+          </form>
+        <Container>
+          <Row>
+            {
+              results.map((employees, i) => (
+                <Column key={i}>
+                  <Card
+                    image={employees.picture.large}
+                    firstName={employees.name.first}
+                    lastName={employees.name.last}
+                    fullname = {employees.name.first + employees.name.last}
+                    age={employees.dob.age}
+                    DOB={employees.dob.date}
+                    phoneNum={employees.phone}
+                    email={employees.email}
+                    ssn={employees.id.value}
+                  />
+                </Column>
+              ))
+            }
+          </Row>
+        </Container>
+      </div>
+      );
+  }
+
+    }
+
+    export default Employees;
