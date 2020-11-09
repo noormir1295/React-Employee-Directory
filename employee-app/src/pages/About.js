25244ee9da0a996b1f0a6796cb1f718d62be4c4d
Noor <noormir1295@gmail.com>
2020-11-09 15:14:02 -0500

    adding wrapper and beggining About page

---
+++ employee-app/src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Column";
+
+function About() {
+    return (
+        <div>
+            
+        </div>
+
+    )
+}
+
+export default About; 
