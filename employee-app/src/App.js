170dae33cd73ca58d6db1d54ee9b650313cbfd56
Noor <noormir1295@gmail.com>
2020-11-09 15:35:07 -0500

    finishing about page

--- employee-app/src/App.js
+++ employee-app/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Wrapper from "./components/Wrapper";
+import About from './pages/About';
+
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <Router>
         <div>
           <Navbar />
+          <Wrapper>
+            <Route exact path='/' component={About} />
+          </Wrapper>
         </div>
       
       </Router>
--- employee-app/src/pages/About.js
+++ employee-app/src/pages/About.js
@@ -7,7 +7,34 @@ import Col from "../components/Column";
 function About() {
     return (
         <div>
-            
+            <Hero backgroundImage=" https://www.ballardspahr.com/-/media/images/offices/backgrounds/saltlakecity.jpg">
+                <h1>Noor LLC</h1>
+                <h2>Employee Directory</h2>
+            </Hero>
+            <Container style={{marginTop: 30}}>
+                <Row>
+                    <Col style={{marginTop:10}} size="md-12">
+                        <h1>  Welcome to our Employee Directory</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col size="md-12">
+                        <p>
+                            Welcome to my first React Application - this application will let a user easily search their compay's employees to find some information about that employee,
+                            such as their contact information.
+            </p>
+                        <br></br>
+                        <p>Technologies Used for this project:</p>
+                        <ul>
+                            <li>React, Framework website is built on</li>
+                            <li>Axios, to make an API call for the random "Employees"</li>
+                            <li>React Router DOM, for the seperate pages</li>
+                        </ul>
+                        <br></br>
+                        <p className="ty"> Thank you for visiting. - Andrew Kessler</p>
+                    </Col>
+                </Row>
+            </Container>
         </div>
 
     )
